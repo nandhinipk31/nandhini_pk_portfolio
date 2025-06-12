7c447096dbb4ffde162cb1ec2c795c332ee6b81c
Nandhini P K <pknandhini10030@gmail.com>
2025-06-12 23:52:45 +0530

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+
+function openModal(projectName) {
+  alert('Show details and screenshots for: ' + projectName);
+}
+function openIntern(companyName) {
+  alert('Show description and certificates for: ' + companyName);
+}
